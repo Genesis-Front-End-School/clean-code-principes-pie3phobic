aabfb615146e0345399514d1aa95b44d9317670e
= <nastya.minty@gmail.com>
2023-04-19 23:24:58 +0300

    added prettier, eslint rules, error handling

--- pages/api/fetchData.js
+++ pages/api/fetchData.js
@@ -1,17 +1,22 @@
 export default async function fetchData() {
-  const host = "http://api.wisey.app";
-  const version = "api/v1";
-  const accessToken = await fetch(
-    `${host}/${version}/auth/anonymous?platform=subscriptions`
-  );
-  const accesData = await accessToken.json();
-  const token = accesData.token;
-  const res = await fetch(`${host}/${version}/core/preview-courses`, {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
-  const data = await res.json();
+  try {
+    const host = "http://api.wisey.app";
+    const version = "api/v1";
+    const accessToken = await fetch(
+      `${host}/${version}/auth/anonymous?platform=subscriptions`
+    );
+    const accesData = await accessToken.json();
+    const token = accesData.token;
+    const res = await fetch(`${host}/${version}/core/preview-courses`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    const data = await res.json();
 
-  return { data: data || null, accesData: accesData || null };
+    return { data: data || null, accesData: accesData || null };
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    throw error;
+  }
 }
