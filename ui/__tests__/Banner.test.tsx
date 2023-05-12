aa6b5a22ba942f057e3a8340fd9f70b8d4300323
= <nastya.minty@gmail.com>
2023-05-13 00:49:50 +0300

    small test fix

--- ui/__tests__/Banner.test.tsx
+++ ui/__tests__/Banner.test.tsx
@@ -3,19 +3,22 @@ import "@testing-library/jest-dom";
 import React from "react";
 import Banner from "../index/Banner";
 describe("Banner", () => {
-  test("renders an image with the specified src and alt text", () => {
+  beforeEach(() => {
     render(<Banner />);
+  });
+
+  test("renders an image with the specified src and alt text", () => {
     const image = screen.getByAltText(
       "Image of student listening to an online course"
     );
     expect(image).toBeInTheDocument();
     expect(image).toHaveAttribute("src", "student-cr.jpg");
   });
+
   test("applies the correct CSS classes", () => {
-    render(<Banner />);
     const card = screen.getByRole("img");
     expect(card).toHaveClass("rounded-3xl");
-    const container = screen.getByRole("img").parentElement;
+    const container = card.parentElement;
     expect(container).toHaveClass("bg-gray-100");
     expect(container).toHaveClass("p-6");
     expect(container).toHaveClass("md:p-10");
