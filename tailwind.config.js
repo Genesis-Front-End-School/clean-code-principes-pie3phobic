b62ef181048b38d54e567c7f1aac2d9431ee2314
= <nastya.minty@gmail.com>
2023-05-15 23:05:50 +0300

    code cleanup2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,14 @@
 module.exports = {
   darkMode: "class",
   content: ["./pages/**/*.{js,ts,jsx,tsx}", "./ui/**/*.{js,ts,jsx,tsx}"],
-  //content: [],
   theme: {
     extend: {
       colors: {
         "blue-950": "#0F172A",
-        midnight: "#121063",
         "gray-950": "#414B5C",
         "purple-accent": "#6366F1",
       },
     },
   },
-  // theme: {
-  //   extend: {},
-  // },
   plugins: [require("tailwind-scrollbar-hide")],
 };
